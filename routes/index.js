67089bf31823ba6e1089e9f1184fd148398c6b81
ovdickson <dickson@cloudplexo.com>
2026-04-17 09:25:26 +0100

    added ecr push

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Hello, Welcome to Contoso!',   
+    title: 'Hello, Welcome to Tech Team Friday!',   
   });
 });
 
